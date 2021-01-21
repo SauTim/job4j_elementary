de12e85a900eb936bbf6c7043260fa3b166bd3ff
99897 <tim1999997@gmail.com>
2021-01-22 02:42:43 +0500

    Multiple.java

---
+++ src/ru/job4j/variables.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+public class variables {
+    public static void main(String[] args) {
+        int age = 18;
+        double length = 78.0332;
+        boolean baby = true;
+        System.out.println(age);
+        System.out.println(length);
+        System.out.println(baby);
+    }
+}
