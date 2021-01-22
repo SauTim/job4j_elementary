3af411c374d0e11ff1e6bb9d164e532ff3474159
99897 <tim1999997@gmail.com>
2021-01-23 02:12:06 +0500

    Multiple.java

---
+++ src/ru/job4j/definevariables.java
@@ -0,0 +1,16 @@
+package ru.job4j;
+
+
+
+public class definevariables {
+    public static void main(String[]args) {
+        byte age = 120;
+        boolean gender = true;
+        float height = 220.3F;
+        int bricksammount = 560000;
+        long bacteriaammount = 380000000000L;
+        double spacedistance = 34515512477.2562;
+        boolean permission = false;
+        long filesize = 2506050075L;
+    }
+}
